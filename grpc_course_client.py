4fe5f5261279d07642df2dc4bfc7bb43f6f1d9b2
mbigvava <mbigvava@at-consulting.ru>
2025-07-01 00:35:50 +0500

    задание по grpc

---
+++ grpc_course_client.py
@@ -0,0 +1,21 @@
+import grpc
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+def run():
+    # Устанавливаем соединение с сервером
+    with grpc.insecure_channel('localhost:50050') as channel:
+        stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+        # Формируем запрос
+        request = course_service_pb2.GetCourseRequest(course_id="api-course")
+
+        # Вызываем метод GetCourse
+        response = stub.GetCourse(request)
+
+        # Выводим ответ
+        print(response)
+
+if __name__ == '__main__':
+    run()
